ea41af3653326f393ad67e48eef701f3077c7e6d
arturha4 <ghghrfth>
2024-04-13 18:43:37 +0500

    api data downloading working with threads

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import sys
+import time
 
 import gi
-from paycon_api import get_api1_data_names
+import threading
+from paycon_api import *
 gi.require_version("Gtk", "3.0")
-from gi.repository import GLib,Gtk
+from gi.repository import GLib,Gtk, Gdk
+from gi.repository import GObject
 
-
-class MyApplication(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
-
-    def do_activate(self):
-        self.window = Gtk.ApplicationWindow(application=self, title="Main window")
-        self.window.resize(800, 600)
-        self.window.present()
-        button_api = Gtk.Button.new_with_label('Загрузить из API')
-        button_csv = Gtk.Button.new_with_label('Загрузить из файла')
-        self.window.add(button_api)
-        # self.window.add(button_csv)
-
-#
-# app = MyApplication()
-# exit_status = app.run(sys.argv)
-# sys.exit(exit_status)
 
 
 class MainWindow(Gtk.Window):
 
     def __init__(self):
+        self.data_storage = []
         Gtk.Window.__init__(self, title="Main Window")
         self.set_default_size(800, 600)
         self.set_resizable(False)
@@ -37,20 +22,45 @@ class MainWindow(Gtk.Window):
     def create_buttons(self):
         button_api = Gtk.Button(label="Загрузить из API")
         button_csv = Gtk.Button(label="Загрузить из файла")
-
-        button_api.connect("clicked", self.get_api_response)
+        button_api.connect("clicked", self.on_button_clicked)
         button_csv.connect("clicked", self.on_button_clicked)
         box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         box.pack_start(button_api, True, True, 50)
         box.pack_start(button_csv, True, True, 50)
         self.add(box)
 
-    def on_button_clicked(self, widget):
-        print("Button clicked!")
 
+    def api_task1(self):
+        self.data_storage.append(get_api_data_names(paycon_url))
 
-    def get_api_response(self,widget):
-        get_api1_data_names()
+    def api_task2(self):
+        self.data_storage.append(get_api_data_names(paycon_url2))
+
+    def show_download_dialog(self):
+        dialog = Gtk.Dialog('Download', None, modal=True, destroy_with_parent=True)
+        spinner = Gtk.Spinner()
+        dialog.vbox.pack_start(spinner, True, True, 0)
+        dialog.show_all()
+        spinner.start()
+        spinner.show()
+        dialog.run()
+        return dialog
+
+    def on_button_clicked(self, dialog):
+        ## не отрисовывается анимация спинера
+        thread_api1 = threading.Thread(target=self.api_task1)
+        thread_api2 = threading.Thread(target=self.api_task2)
+        thread_api1.start()
+        thread_api2.start()
+        dialog = Gtk.Dialog('Download', None, modal=True, destroy_with_parent=True)
+        spinner = Gtk.Spinner()
+        dialog.vbox.pack_start(spinner, True, True,0)
+        dialog.show_all()
+        thread_api1.join()
+        thread_api2.join()
+        # print(self.data_storage)
+        dialog.hide()
+
 
 win = MainWindow()
 win.create_buttons()
--- paycon_api.py
+++ paycon_api.py
@@ -1,12 +1,13 @@
+import time
 import urllib.request
-from conf import paycon_url
+from conf import paycon_url, paycon_url2
 import ssl, json
 
-def get_api1_data_names(url: str = paycon_url):
+def get_api_data_names(url: str = paycon_url):
     ssl._create_default_https_context = ssl._create_unverified_context
     try:
         json_bin = urllib.request.urlopen(paycon_url)
         data = json.load(json_bin)
-        print(data)
+        return data
     except ConnectionError:
         return
